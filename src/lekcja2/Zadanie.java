dea19d05e9cc4c19ad236e767e97cf9f57c5c63f
Przemek <przemyslawjakubowski@gmail.com>
2021-04-19 11:14:50 +0200

    Lekcja 2

---
+++ src/lekcja2/Zadanie.java
@@ -0,0 +1,18 @@
+package lekcja2;
+
+public class Zadanie {
+
+//    1. Napisz kod, który doda to siebie dwie liczby i wypisze wynik dodawania
+//    2. Napisz kod, który podzieli dwie liczby i wypisze wynik dzielenia
+//    3. Napisz program liczący pole trójkąta. Zadeklaruj zmienne do tego potrzebne rezultat zapisz w kolejnej zmiennej.
+//    4. Napisz program liczący pole kwadratu. Zadeklaruj zmienne do tego potrzebne rezultat zapisz w kolejnej zmiennej.
+//    5*. Napisz program liczący pole koła. Zadeklaruj odpowiednią zmienną do przechowywania promienia oraz rezultatu.
+//          Pomyśl jak przechowywać PI i czy możnaby tej wartości nie wpisywać "z palca"?
+//    6.
+//// Napisz program rozwiązujący równania kwadratowe (o ile istnieje)
+////        https://www.matemaks.pl/rownania-kwadratowe-w-postaci-ogolnej.html
+
+    public static void main(String[] args) {
+        Math.sqrt(4);
+    }
+}
