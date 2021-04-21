c62406fd8b79cc49e2f6233fbc138f3985c61271
Przemek <przemyslawjakubowski@gmail.com>
2021-04-21 12:24:04 +0200

    Lekcja 8 zadanie

---
+++ src/lekcja8/Zadanie.java
@@ -0,0 +1,16 @@
+package lekcja8;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+
+public class Zadanie {
+    public static void main(String[] args) throws IOException {
+
+
+        // Oblicz ile razy w Bibli występuje słowo Pan
+        // Oblicz ile linii ma Biblia
+
+        // Biblia znajduje się w pliku PBG
+    }
+}
