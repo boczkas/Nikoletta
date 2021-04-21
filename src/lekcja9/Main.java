7fd28a31414a5242c8ddc1041a5cce986d47b06d
Przemek <przemyslawjakubowski@gmail.com>
2021-04-21 12:00:13 +0200

    Lekcja 8

---
+++ src/lekcja9/Main.java
@@ -0,0 +1,13 @@
+package lekcja9;
+
+public class Main {
+    public static void main(String[] args) {
+        //        1. Typ wyliczeniowy
+//        Stworzyć dni tygodnia
+//        DzienTygodnia dzisiaj = DzienTygodnia.PONIEDZIALEK;
+//
+//        if (dzisiaj.equals(DzienTygodnia.PONIEDZIALEK)) {
+//            System.out.println("Dzisiaj poniedzialek");
+//        }
+    }
+}
