b9d875707b25a7be133bdd439d1d6aa75d91e0df
Przemek <przemyslawjakubowski@gmail.com>
2021-04-21 10:01:36 +0200

    Lekcja 5

---
+++ src/lekcja5.java
@@ -0,0 +1,29 @@
+public class lekcja5 {
+    public static void main(String[] args) {
+        //    1. Instrukcje przerywające przepływ sterowania
+//    break;
+//    continue;
+
+        int i = 0;
+
+        while (i < 10) {
+            i++;
+            if (i % 2 == 0) {
+                continue;
+            }
+            System.out.println("i = " + i);
+        }
+
+        System.out.println("==============");
+
+        i = 0;
+
+        while (i < 10) {
+            i++;
+            if (i > 5) {
+                break;
+            }
+            System.out.println("i = " + i);
+        }
+    }
+}
