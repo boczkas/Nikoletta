8243db9a399cf6f75461a71d198a8d0aef71162f
Przemek <przemyslawjakubowski@gmail.com>
2021-05-17 09:19:34 +0200

    Lekcja 12

---
+++ src/lekcja12/Main.java
@@ -0,0 +1,12 @@
+package lekcja12;
+
+public class Main {
+    //        Zaprojektuj system do przyjmowania zamówień w restauracji
+
+//        1. Zamówienie ma w sobie dania.
+//        2. Każde danie ma swoją cenę i nazwę
+//        3. Zamówienie ma cenę całkowitą, na którą składają się ceny
+//          poszczególnych zamówień
+//        4. Powinna istnieć możliwość dodania napiwku do zamówienia
+//        5. Powinna istnieć możliwość usunięcia dania z zamówienia
+}
--- src/zadanieOX/Main.java
+++ src/zadanieOX/Main.java
@@ -11,12 +11,20 @@ public class Main {
 //        Wymagania:
 //        1. Gra ma być na dwóch graczy
 //        2. Rozmiar planszy ma być konfigurowany na starcie gry
-//        3. Gracz wygrywa, gdy ma 3 swoje symbole obok siebie w dowolnej konfiguracji - pion, poziom, skos
-//        4. Gracze mają wprowadzać wybór swojego pola jeden po drugim
-//        5. Program ma znajdować moment w którym jeden z graczy zwycięża
-//        6. Można zakończyć grę w dowolnym momencie po wpisaniu słowa quit
+//        3. Gracze mają mieć możliwość wprowadzenia swojego imienia
+//        4. Gracz wygrywa, gdy ma 3 swoje symbole obok siebie w dowolnej konfiguracji - pion, poziom, skos
+//        5. Gracze mają wprowadzać wybór swojego pola jeden po drugim
+//        6. Program ma znajdować moment w którym jeden z graczy zwycięża
 //        7. Gra ma mieć 3 rundy
 //        8. Zaczynający turę gracz ma się zmieniać co rozgrywkę
+//        9. Można zakończyć grę w dowolnym momencie po wpisaniu słowa quit
+//        10. Gra ma informować jakie dane powinny być wprowadzone.
+
+//        Możliwe rozszerzenia:
+//          1. Gra na wielu graczy
+//          2. Możliwość grania dowolnym symbolem
+//          3. Możliwość zdefiniowania ile symboli obok siebie daje zwycięstwo
+
 
 
         Player currentPlayer = player1;
